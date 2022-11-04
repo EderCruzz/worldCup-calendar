e70d2cc881ebfe94e3b0ebbd823f6a85eb91673c
Eder Cruz <eder.ales10@gmail.com>
2022-11-04 11:27:58 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+
+function createGame(player1, hour, player2){
+    return `<li>
+            <img src="./assets/logo-${player1}.svg" alt="Bandeira do ${player1}">
+            <strong>${hour}</strong>
+            <img src="./assets/logo-${player2}.svg" alt="Bandeira do ${player2}">
+        </li>`
+}
+
+let delay = -0.2
+
+function createCard(date, day, games){
+    delay = delay + 0.2
+    return `
+    <div class="card" style="animation-delay: ${delay}s">
+    <h2>${date} <span>${day}</span></h2>
+    <ul>
+        ${games}
+    </ul>
+</div>`
+}
+
+
+document.querySelector('.cards').innerHTML = 
+    
+    createCard('24/11', 'quinta', 
+    createGame('brazil', '16:00', 'serbia'))
+    +
+    createCard('28/11', 'segunda', 
+    createGame('brazil', '13:00', 'switzerland') + 
+    createGame('portugal', '16:00', 'uruguay'))
+    +
+    createCard('02/12', 'sexta', 
+    createGame('cameroon', '16:00', 'brazil') )
+
+
